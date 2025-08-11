d4ff977c56a4d07532d3cdf7c11cab57d308e487
Rubick <andrew.fox.3.14@gmail.com>
2025-08-11 23:26:52 +0300

    Add changes

--- etl.py
+++ etl.py
@@ -1,5 +1,22 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+# Загрузка переменных окружения из .env файла
+load_dotenv()
+
+# Получение данных из переменных окружения
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Подключение к PostgreSQL
+engine = create_engine(
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 # Загрузка данных
 sales = pd.read_csv(
@@ -8,18 +25,11 @@ sales = pd.read_csv(
     parse_dates=["sale_date"]
 )
 
-
 # Очистка данных
 sales_clean = sales.dropna().copy()
 
-
 # Вычисление total
 sales_clean["total"] = sales_clean["price"] * sales_clean["quantity"]
-
-
-# Подключение к postgresql
-engine = create_engine("postgresql+psycopg2://postgres:This1is2just3a4test!@localhost:5432/sales_db")
-
 
 # Создание таблицы (если её нет)
 create_table_sql = """
@@ -33,12 +43,11 @@ CREATE TABLE IF NOT EXISTS sales (
     total NUMERIC(10,2)
 );
 """
+
 with engine.connect() as conn:
     conn.execute(text(create_table_sql))
 
-
-# 6. Загрузка данных в БД
+# Загрузка данных в БД
 sales_clean.to_sql("sales", engine, if_exists="append", index=False)
 
 print("\nДанные успешно загружены в PostgreSQL!")
-
